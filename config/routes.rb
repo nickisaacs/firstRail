8fd732f81d27a4347f67a41e4c7c7ad4910c74ab
Nick Isaacs <nisaacs@splickit.com>
2012-11-04 18:34:46 -0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 FirstRail::Application.routes.draw do
-  get "static_pages/home"
+  get "/static_pages/home"
 
-  get "static_pages/help"
+  get "/static_pages/help"
 
-  get "static_pages/about"
+  get "/static_pages/about"
 
   resources :microposts
 
